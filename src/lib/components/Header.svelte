816e2eace3d454ccd891845868446c57ad29b1d6
Martz <github@jmartz.de>
2023-03-03 21:10:08 +0100

    update content, renmae url routes, add mobile navigation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,9 @@
 <script>
+  import { faBars } from "@fortawesome/free-solid-svg-icons";
   import { onMount } from "svelte";
   import { init, isLoggedIn } from "../../stores/user";
+  import Fa from "svelte-fa";
+  import { open } from "../../stores/navigation";
 
   onMount(function () {
     init();
@@ -25,4 +28,27 @@
       {/if}
     </ul>
   </nav>
+  <nav class="md:hidden">
+    <button
+      on:click={function () {
+        open.set(!$open);
+      }}><Fa icon={faBars} class="mt-1" size="1.5x" /></button
+    >
+  </nav>
 </header>
+<section class="mobile-content px-2 py-2 bg-white mx-2 my-2 md:hidden">
+  <div class="grid grid-cols-8 gap-3">
+    <div class="col-span-4 sm:col-span-2"><a href="/settings">Settings</a></div>
+    <div class="col-span-4 sm:col-span-2"><a href="/servers">Servers</a></div>
+    <div class="col-span-4 sm:col-span-2"><a href="/ssh-keys">SSH Keys</a></div>
+    <div class="col-span-4 sm:col-span-2">
+      <a href="/certificates">Certs</a>
+    </div>
+    <div class="col-span-4 sm:col-span-2"><a href="/volumes">Volumes</a></div>
+    <div class="col-span-4 sm:col-span-2">
+      <a href="/load-balancers">Load Balancer</a>
+    </div>
+    <div class="col-span-4 sm:col-span-2"><a href="/networks">Networks</a></div>
+    <div class="col-span-4 sm:col-span-2"><a href="/logout">Logout</a></div>
+  </div>
+</section>
